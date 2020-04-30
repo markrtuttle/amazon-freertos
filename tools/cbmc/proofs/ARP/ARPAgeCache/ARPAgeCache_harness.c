3b34d4d98f4dcb42b1dbcd31f26fd9f6b5230132
Mark R. Tuttle <mrtuttle@amazon.com>
2020-04-30 14:35:04 -0400

    Repair CBMC proof patches, MakefileCommon.json to build CBMC proofs.
    This patch also modifies ARPArgCache harness and the include/windows
    stubs, but the proofs are still failing and need to be debugged.

--- tools/cbmc/proofs/ARP/ARPAgeCache/ARPAgeCache_harness.c
+++ tools/cbmc/proofs/ARP/ARPAgeCache/ARPAgeCache_harness.c
@@ -1,11 +1,25 @@
+/* Standard includes. */
+#include <stdint.h>
+#include <stdio.h>
+
 /* FreeRTOS includes. */
 #include "FreeRTOS.h"
-#include "list.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
 
 /* FreeRTOS+TCP includes. */
 #include "FreeRTOS_IP.h"
+#include "FreeRTOS_Sockets.h"
 #include "FreeRTOS_IP_Private.h"
 #include "FreeRTOS_ARP.h"
+#include "FreeRTOS_UDP_IP.h"
+#include "FreeRTOS_DHCP.h"
+#if( ipconfigUSE_LLMNR == 1 )
+	#include "FreeRTOS_DNS.h"
+#endif /* ipconfigUSE_LLMNR */
+#include "NetworkBufferManagement.h"
+#include "NetworkInterface.h"
 
 //We assume that the pxGetNetworkBufferWithDescriptor function is implemented correctly and returns a valid data structure.
 //This is the mock to mimic the correct expected bahvior. If this allocation fails, this might invalidate the proof.
