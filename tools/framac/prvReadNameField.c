b0f95ce22da940061d918e58b92ac4e5eb823b35
Mark R. Tuttle <mrtuttle@amazon.com>
2020-07-18 01:36:54 +0000

    Nested loop examples

--- tools/framac/prvReadNameField.c
+++ tools/framac/prvReadNameField.c
@@ -4,7 +4,7 @@
 #define dnsNAME_IS_OFFSET					( ( uint8_t ) 0xc0 )
 
 /*@
-    predicate is_size_t(size_t n) = 
+    predicate is_size_t(size_t n) =
         0 <= n <= SIZE_MAX;
 */
 
@@ -13,90 +13,72 @@
     requires is_size_t(uxRemainingBytes);
     requires is_size_t(uxDestLen);
     requires \valid(pcName + (0 .. uxDestLen - 1));
-    assigns pcName[0 .. uxDestLen - 1];
-
-    ensures uxRemainingBytes == ( size_t ) 0U ==> \result == 0U;
+    requires \separated(pcName, pucByte);
 */
-static size_t prvReadNameField( const uint8_t *pucByte,
+static size_t prvReadNameField( const uint8_t * const pucByte,
                                 size_t uxRemainingBytes,
-                                char *pcName,
+                                char * const pcName,
                                 size_t uxDestLen )
 {
 size_t uxNameLen = 0U;
 size_t uxIndex = 0U;
 size_t uxSourceLen = uxRemainingBytes;
 
-/* uxCount gets the valus from pucByte and counts down to 0.
-No need to have a different type than that of pucByte */
-size_t uxCount;  
-
+uint8_t uxCount;
     if( uxSourceLen == ( size_t ) 0U )
     {
-        /* Return 0 value in case of error. */
         uxIndex = 0U;
-    }
-    /* Determine if the name is the fully coded name, or an offset to the name
-    elsewhere in the message. */
-    else if( ( pucByte[ uxIndex ] & dnsNAME_IS_OFFSET ) == dnsNAME_IS_OFFSET )
-    {
-        /* Jump over the two byte offset. */
-        if( uxSourceLen > sizeof( uint16_t ) )
-        {
-            uxIndex += sizeof( uint16_t );
-        }
-        else
-        {
-            uxIndex = 0U;
-        }
     }
     else
     {
-        //@ assert uxSourceLen > 0 && uxDestLen > 0;
-        /* 'uxIndex' points to the full name. Walk over the string. */
+        //@ assert uxSourceLen > 0;
+        //@ assert uxIndex < uxSourceLen;
+        //@ assert 0 <= uxIndex <= uxSourceLen;
+        //@ assert \forall size_t j; 0 <= j < uxIndex ==> pucByte[j] != ( uint8_t )0x00U;
+
+      //            loop invariant \forall size_t j; 0 <= j < uxIndex ==> pucByte[j] != ( uint8_t )0x00U;
+
+
         /*@
             loop invariant 0 <= uxIndex <= uxSourceLen;
-            loop invariant \forall size_t j; 0 <= j < uxIndex ==> pucByte[j] != ( uint8_t )0x00U;
-            loop assigns pcName[0 .. uxDestLen - 1], uxNameLen, uxIndex, uxCount;
+            loop invariant 0 <= uxNameLen <= uxDestLen;
+	    loop invariant \separated(pcName, pucByte);
+            loop assigns pcName[0 .. uxDestLen], uxNameLen, uxIndex, uxCount, uxRemainingBytes, uxDestLen;
             loop variant uxSourceLen - uxIndex;
         */
         while( ( uxIndex < uxSourceLen ) && ( pucByte[ uxIndex ] != ( uint8_t )0x00U ) )
         {
-            /* If this is not the first time through the loop, then add a
-            separator in the output. */
             if( ( uxNameLen > 0U ) )
             {
                 if( uxNameLen >= uxDestLen )
                 {
                     uxIndex = 0U;
-                    /* coverity[break_stmt] : Break statement terminating the loop */
                     break;
                 }
                 pcName[ uxNameLen ] = '.';
                 uxNameLen++;
             }
-
-            /* Process the first/next sub-string. */
-            uxCount = ( size_t ) pucByte[ uxIndex ];
+            uxCount = ( uint8_t ) pucByte[ uxIndex ];
+	    //@ assert 0 <= uxCount < 256;
             uxIndex++;
             if( ( uxIndex + uxCount ) > uxSourceLen )
             {
                 uxIndex = 0U;
                 break;
             }
-      //      loop invariant 0 <= uxCount;
             /*@
                 loop invariant 0 <= uxIndex <= uxSourceLen;
-                loop assigns pcName[\at(uxNameLen, LoopEntry) .. uxDestLen - 1], uxNameLen, uxIndex, uxCount;
+                loop invariant 0 <= uxNameLen <= uxDestLen;
+   	        loop invariant \separated(pcName, pucByte);
+                loop assigns pcName[0 .. uxDestLen], uxNameLen, uxIndex, uxCount, uxRemainingBytes, uxDestLen;
                 loop variant uxCount;
-            */               
+            */
             while( ( uxCount-- != 0U ) && ( uxIndex < uxSourceLen ) )
             {
                 if( uxNameLen >= uxDestLen )
                 {
                     uxIndex = 0U;
                     break;
-                    /* break out of inner loop here
-                    break out of outer loop at the test uxNameLen >= uxDestLen. */
                 }
                 pcName[ uxNameLen ] = ( char ) pucByte[ uxIndex ];
                 uxNameLen++;
@@ -104,7 +86,6 @@ size_t uxCount;
             }
         }
 
-        /* Confirm that a fully formed name was found. */
         if( uxIndex > 0U )
         {
             if( ( uxNameLen < uxDestLen ) && ( uxIndex < uxSourceLen ) && ( pucByte[ uxIndex ] == 0U ) )
