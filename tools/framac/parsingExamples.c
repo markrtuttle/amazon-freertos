d681f4e3b0f1a6d502c4ca1627b31383699c1a21
Isabella Fulford <fulfoi@amazon.com>
2020-08-04 16:44:56 +0000

    parsing examples finished

--- tools/framac/parsingExamples.c
+++ tools/framac/parsingExamples.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <limits.h>
 
 struct Message {
   uint16_t A;
@@ -10,24 +11,22 @@ typedef struct Message Message_t;
 
 /*@
     predicate is_uint8_t(integer n) =
-        0 <= n < 1<<8;
+        0 <= n <= UCHAR_MAX;
     
     predicate is_uint16_t(integer n) =
-        0 <= n < 1<<16;
-
-    predicate max_shifted(integer n) = 
-        0 <= n < (1<<16 - 1<<8 + 1 );
+        0 <= n <= USHRT_MAX;
 */
 
 /*@
     requires \valid(buffer + (0 .. bufferLength - 1));
-    requires \forall int i; 0 <= i < bufferLength ==> is_uint8_t(*(buffer + i));
-    requires \forall int i; 0 <= i < bufferLength ==> is_uint16_t(*(buffer + i) << 8);
+    requires \forall uint16_t i; 0 <= i < bufferLength ==> is_uint8_t(*(buffer + i));
+    requires \forall uint16_t i; 0 <= i < bufferLength ==> is_uint16_t(*(buffer + i) * 2^8);
+    requires is_uint16_t(bufferLength);
 
     assigns \nothing;
 
     ensures bufferLength <= sizeof(Message_t) ==> \result == 0;
-    //ensures bufferLength > sizeof(Message_t) ==> \result == (uint16_t)(*buffer << 8 + *(buffer + 1));
+    ensures bufferLength > sizeof(Message_t) ==> \result == (uint16_t)(*buffer * 2^8 + *(buffer + 1));
 */
 uint16_t parseBuffer(uint8_t *buffer, uint16_t bufferLength) {
     uint8_t *pucByte = buffer;
@@ -37,9 +36,17 @@ uint16_t parseBuffer(uint8_t *buffer, uint16_t bufferLength) {
     }
     //@assert is_uint8_t(*pucByte);
     //@assert is_uint8_t(*(pucByte + 1));
-    //@assert is_uint16_t(*pucByte << 8);
-    //@assert max_shifted(*pucByte << 8);
-    //@assert is_uint16_t(*pucByte << 8 + (1<<8 - 1));
-    uint16_t A = *pucByte << 8 +  *(pucByte + 1);
+    //@assert is_uint16_t(*pucByte * 2^8);
+    uint16_t A = *pucByte * 2^8 +  *(pucByte + 1);
     return A;
-}                                    
+}   
+
+/*@
+    requires is_uint8_t(a);
+    assigns \nothing;
+*/
+uint16_t foo(uint8_t a) {
+
+    uint16_t A = a * 2^8 + (2^8 - 1);
+    return A;
+}
